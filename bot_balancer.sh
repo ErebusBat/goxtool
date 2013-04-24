7829f8c7f1c519d740ff1fea5eccbbe6ad2dc36c
Andrew Burns <andrew@wyominghealthfairs.com>
2013-04-24 10:41:16 -0600

    Backup log file before starting

--- bot_balancer.sh
+++ bot_balancer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 # https://bitcointalk.org/index.php?topic=148462.msg1854636#msg1854636
 # https://gist.github.com/prof7bit/5395900
 # https://bitcointalk.org/index.php?topic=148462.msg1886914#msg1886914
@@ -10,5 +10,9 @@
 # "b":balance immediately (cancel orders and then buy/sell at market to rebalance)
 #./goxtool.py --protocol=socketio --use-http --strategy=_balancer.py
 
+# Backup log file
+if [[ -f goxtool.log ]]; then 
+  mv goxtool.log goxtool_$(date '+%Y%m%d-%H%M%S').log
+fi
 # Per https://bitcointalk.org/index.php?topic=181584.msg1923260#msg1923260
 ./goxtool.py --protocol=websocket --strategy=_balancer.py
